9d12e307101363ee62605cdd1300165bd6efc58f
Kevin Dickerson <kevin.dickerson@loom.technology>
2016-03-17 15:11:03 -0500

    Increase major version since we renamed everything

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name 'example_resource_cookbook'
+name 'example_resources'
 maintainer 'Kevin Dickerson'
 maintainer_email 'kdickerson@chef.io'
 license 'all_rights'
-description 'Installs/Configures example_resource_cookbook'
-long_description 'Installs/Configures example_resource_cookbook'
-version '0.1.0'
+description 'Installs/Configures example_resources'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '1.0.0'
 
 supports 'centos'
 
-source_url 'https://github.com/kevindickerson/example_resource_cookbook'
-issues_url 'https://github.com/kevindickerson/example_resource_cookbook/issues'
+source_url 'https://github.com/kevindickerson-cookbooks/example_resources'
+issues_url 'https://github.com/kevindickerson-cookbooks/example_resources/issues'
