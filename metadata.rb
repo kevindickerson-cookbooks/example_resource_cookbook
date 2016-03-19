9b7d90d0bf40ab270c1f0aa5e191b7a7e15c5932
Kevin J. Dickerson <kevin.dickerson@loom.technology>
2016-03-19 14:48:50 -0500

    Improve description.

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,9 @@ name 'example_resources'
 maintainer 'Kevin Dickerson'
 maintainer_email 'kevin.dickerson@loom.technology'
 license 'Apache 2.0'
-description 'Provider cookbook with a simple example Chef resource'
+description 'Provider cookbook defining Chef resource. Tests with InSpec.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.4'
+version '1.0.5'
 
 supports 'centos'
 
