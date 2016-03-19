35ec9242951dde96728f4d8eb9ebeee21038d217
Kevin J. Dickerson <kevin.dickerson@loom.technology>
2016-03-19 14:46:40 -0500

    Bump patch version to please Supermarket

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kevin.dickerson@loom.technology'
 license 'Apache 2.0'
 description 'Provider cookbook with a simple example Chef resource'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.3'
+version '1.0.4'
 
 supports 'centos'
 
